64e3967d5583cccc04d662584064a165d479f70d
Alexey Yamshanov <a.yamshanov@gmail.com>
2021-12-21 20:11:15 +0100

    day 21. part 2

--- src/main/kotlin/day21/Day21.kt
+++ src/main/kotlin/day21/Day21.kt
@@ -1,16 +1,13 @@
 package day21
 
-class Player(start: Int) {
-    private var position = start
-    var score = 0
-        private set
-    val won: Boolean
-        get() = score >= 1000
+data class Player(val position: Int, val rolling: Boolean, val score: Int = 0, val rolls: List<Int> = emptyList()) {
+    fun roll(value: Int): Player = if (rolls.size < 2) {
+        Player(position, true, score, rolls + value)
+    } else {
+        val moves = rolls.sum() + value
+        val newPosition = (position + moves - 1) % 10 + 1
 
-    fun rollAndMove(die: DeterministicDie) {
-        val moves = die.take(3).sum()
-        position = (position + moves - 1) % 10 + 1
-        score += position
+        Player(newPosition, false, score + newPosition, emptyList())
     }
 }
 
@@ -32,22 +29,71 @@ class DeterministicDie : Sequence<Int> {
     }
 }
 
-fun main() {
-    val player1 = Player(8)
-    val player2 = Player(6)
+class Game1(player1: Player, player2: Player) {
+    private val scoreToWin = 1000
     val die = DeterministicDie()
+    var player1 = player1
+        private set
+    var player2 = player2
+        private set
 
-    println(part1(player1, player2, die))
+    fun play(): Int {
+        while (true) {
+            repeat(3) { player1 = player1.roll(die.first()) }
+            if (player1.score >= scoreToWin) break
+            repeat(3) { player2 = player2.roll(die.first()) }
+            if (player2.score >= scoreToWin) break
+        }
+        val loser = if (player1.score < scoreToWin) player1 else player2
+
+        return loser.score * die.rolledTimes
+    }
 }
 
-fun part1(player1: Player, player2: Player, die: DeterministicDie): Int {
-    while (true) {
-        player1.rollAndMove(die)
-        if (player1.won) break
-        player2.rollAndMove(die)
-        if (player2.won) break
-    }
-    val loser = if (player1.won) player2 else player1
+class Game2(private val player1: Player, private val player2: Player) {
+    private val scoreToWin = 21
+    private val cache = HashMap<Pair<Player, Player>, Pair<Long, Long>>()
 
-    return loser.score * die.rolledTimes
+    fun play(): Long {
+        val result = splitUniverse(player1, player2)
+        return if (result.first > result.second) result.first else result.second
+    }
+
+    private fun splitUniverse(player1: Player, player2: Player): Pair<Long, Long> {
+        val players = player1 to player2
+        if (players in cache) return cache.getValue(players)
+
+        if (player1.score >= scoreToWin) {
+            val result = 1L to 0L
+            cache[players] = result
+            return result
+        }
+        if (player2.score >= scoreToWin) {
+            val result = 0L to 1L
+            cache[players] = result
+            return result
+        }
+
+        val results = if (player1.rolling) {
+            val cases = (1..3).map { player1.roll(it) }
+            val p2 = if (cases.first().rolling) player2 else player2.copy(rolling = true)
+            cases.map { splitUniverse(it, p2) }
+        } else {
+            val cases = (1..3).map { player2.roll(it) }
+            val p1 = if (cases.first().rolling) player1 else player1.copy(rolling = true)
+            cases.map { splitUniverse(p1, it) }
+        }
+        val result = results.reduce { acc, pair -> acc.first + pair.first to acc.second + pair.second }
+        cache[players] = result
+
+        return result
+    }
+}
+
+fun main() {
+    val player1 = Player(8, true)
+    val player2 = Player(6, false)
+
+    println(Game1(player1, player2).play())
+    println(Game2(player1, player2).play())
 }
--- src/test/kotlin/day21/Day21KtTest.kt
+++ src/test/kotlin/day21/Day21KtTest.kt
@@ -5,47 +5,56 @@ import org.junit.jupiter.api.Test
 
 internal class Day21KtTest {
 
+    private val player1 = Player(4, true)
+    private val player2 = Player(8, false)
+
     @Test
     fun rollAndMove() {
         val die = DeterministicDie()
-        val player1 = Player(4)
-        val player2 = Player(8)
+        var player1 = player1
+        var player2 = player2
 
-        player1.rollAndMove(die)
+        repeat(3) { player1 = player1.roll(die.first()) }
         assertEquals(10, player1.score)
 
-        player2.rollAndMove(die)
+        repeat(3) { player2 = player2.roll(die.first()) }
         assertEquals(3, player2.score)
 
-        player1.rollAndMove(die)
+        repeat(3) { player1 = player1.roll(die.first()) }
         assertEquals(14, player1.score)
 
-        player2.rollAndMove(die)
+        repeat(3) { player2 = player2.roll(die.first()) }
         assertEquals(9, player2.score)
 
-        player1.rollAndMove(die)
+        repeat(3) { player1 = player1.roll(die.first()) }
         assertEquals(20, player1.score)
 
-        player2.rollAndMove(die)
+        repeat(3) { player2 = player2.roll(die.first()) }
         assertEquals(16, player2.score)
 
-        player1.rollAndMove(die)
+        repeat(3) { player1 = player1.roll(die.first()) }
         assertEquals(26, player1.score)
 
-        player2.rollAndMove(die)
+        repeat(3) { player2 = player2.roll(die.first()) }
         assertEquals(22, player2.score)
     }
 
     @Test
     fun part1() {
-        val die = DeterministicDie()
-        val player1 = Player(4)
-        val player2 = Player(8)
+        val game = Game1(player1, player2)
 
-        val result = part1(player1, player2, die)
-        assertEquals(1000, player1.score)
-        assertEquals(745, player2.score)
-        assertEquals(993, die.rolledTimes)
+        val result = game.play()
+        assertEquals(1000, game.player1.score)
+        assertEquals(745, game.player2.score)
+        assertEquals(993, game.die.rolledTimes)
         assertEquals(739785, result)
+    }
+
+    @Test
+    fun part2() {
+        val game = Game2(player1, player2)
+
+        val result = game.play()
+        assertEquals(444356092776315, result)
     }
 }
